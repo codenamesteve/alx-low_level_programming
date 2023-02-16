f1230f6f6c4b512b663dfb61eab3b4da6baae75d
Stephen <stephenaluya@gmail.com>
2023-02-16 15:55:41 -0800

    Trying print without print:D

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-variable"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+write(STDOUT_FILENO, message, sizeof(message)-1);
+
+return (1);
+}
+
+#pragma GCC diagnostic pop
