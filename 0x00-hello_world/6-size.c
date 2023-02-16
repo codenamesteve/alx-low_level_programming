d093160577aa6dfa7819bed5a6f8c7704b802998
Stephen <stephenaluya@gmail.com>
2023-02-16 13:43:49 -0800

    print size in c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char:", sizeof(char));
+printf("Size of an int:", sizeof(int));
+printf("Size of a long int:", sizeof(long));
+printf("Size of a long long int:", sizeof(long long));
+return (0);
+}
