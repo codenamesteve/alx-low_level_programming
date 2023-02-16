effbee310da6c36d41840df8b69c4621908c1057
Stephen <stephenaluya@gmail.com>
2023-02-16 13:10:13 -0800

    Using printf to print out in c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-variable"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * compile without warning
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+}
+
+#pragma GCC diagnostic pop
