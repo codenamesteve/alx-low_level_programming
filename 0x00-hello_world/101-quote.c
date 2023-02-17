787c66544cc58b8a4b4a8a308fbf42de66f0b39b
Stephen <stephenaluya@gmail.com>
2023-02-16 16:16:22 -0800

    Trying print without print:D

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-const char m[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(STDOUT_FILENO, m, sizeof(m) - 1);
+char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDOUT_FILENO, msg, sizeof(msg) - 1);
 
 return (1);
 }
