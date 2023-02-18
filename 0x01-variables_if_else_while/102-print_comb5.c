58496d0852d526f85a48f4494e969cb04012675e
Stephen <stephenaluya@gmail.com>
2023-02-17 20:54:55 -0800

    putchar() rules

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,11 +11,11 @@ for (i = 0; i < 100; i++)
 {
 for (j = i + 1; j < 100; j++)
 {
-putchar('0' + (i / 10);
-putchar('0' + (i % 10);
+putchar('0' + (i / 10));
+putchar('0' + (i % 10));
 putchar(32);
-putchar('0' + (j / 10);
-putchar('0' + (j % 10);
+putchar('0' + (j / 10));
+putchar('0' + (j % 10));
 if (i != 98 && j != 99)
 {
 putchar(44);
