893b3d10ec159c124e5f1fdaa8a36672298b1868
Stephen <stephenaluya@gmail.com>
2023-02-17 20:52:38 -0800

    putchar() rules

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: 0 sucess
+ */
+int main(void)
+{
+int i, j;
+for (i = 0; i < 100; i++)
+{
+for (j = i + 1; j < 100; j++)
+{
+putchar('0' + (i / 10);
+putchar('0' + (i % 10);
+putchar(32);
+putchar('0' + (j / 10);
+putchar('0' + (j % 10);
+if (i != 98 && j != 99)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+putchar('\n');
+return (0);
+}
