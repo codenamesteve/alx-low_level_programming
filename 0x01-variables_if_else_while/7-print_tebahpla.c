8626ca2e913f4ab0b37d6ed1cdcb1d36d20d6605
Stephen <stephenaluya@gmail.com>
2023-02-17 19:08:51 -0800

    putchar() is cool

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: 0 sucess
+ */
+int main(void)
+{
+char n;
+for (n = 'z'; n >= 'a'; n--)
+{
+putchar(n);
+}
+putchar('\n');
+
+return (0);
+}
