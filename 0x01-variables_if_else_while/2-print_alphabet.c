1a3da911308a4244faa58e783ef2114d482eea08
Stephen <stephenaluya@gmail.com>
2023-02-17 18:15:49 -0800

    Trying something new

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: 0 sucess
+ */
+int main(void)
+{
+char alphabet = [a..z];
+
+putchar(alphabet);
+putchar("\n");
+
+return (0);
+}
