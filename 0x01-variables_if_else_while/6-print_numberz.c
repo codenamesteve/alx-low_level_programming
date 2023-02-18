cf1fea5ad9abdd798f55a0cb32ef3129b3302003
Stephen <stephenaluya@gmail.com>
2023-02-17 19:05:30 -0800

    putchar() is cool

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: 0 sucess
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar('0' + i);
+}
+putchar('\n');
+return (0);
+}
