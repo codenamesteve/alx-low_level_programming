65497bd9e8568ca68f0e74826af198f8ed0b0501
Stephen <stephenaluya@gmail.com>
2023-02-17 20:17:23 -0800

    putchar() is cool

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: 0 sucess
+ */
+int main(void)
+{
+  int i, j, k;
+for (i = 48; i < 58; i++)
+{
+for (j = i + 1; j < 58; j++)
+{
+for (k = j + 1; k < 58; k++)
+{
+putchar(i);
+putchar(j);
+putchar(k);
+if (i != 55 || j != 56 || k != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+putchar('\n');
+return (0);
+}
