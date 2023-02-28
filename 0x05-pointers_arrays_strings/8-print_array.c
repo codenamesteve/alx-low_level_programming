bceb71eda3e93650712d2befc3292bcaf28fbd9c
Stephen <stephenaluya@gmail.com>
2023-02-28 15:45:24 -0800

    done this befoe putchar

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,15 +10,13 @@ void print_array(int *a, int n)
 int i;
 for (i = 0; i < n; i++)
 {
-if(i != (n - 1))
+if (i != (n - 1))
 {
-putchar(a[i]);
-putchar(',');
-putchar(' ');
+printf("%d, ", a[i]);
 }
 else
 {
-putchar(a[i]);
+printf("%d", a[i]);
 }
 }
 putchar('\n');
