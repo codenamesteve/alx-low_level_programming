7f522c3c97faad40840a0597937c773345ff7d74
Stephen <stephenaluya@gmail.com>
2023-02-28 15:57:29 -0800

    done this befoe putchar

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcpy - Main Code Insertion
+ * @a: char type pointer.
+ * @n: char type pointer.
+*/
+char *_strcpy(char *dest, char *src)
+{
+size_t i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+for ( ; i < n; i++)
+dest[i] = '\0';
+return dest;
+}
