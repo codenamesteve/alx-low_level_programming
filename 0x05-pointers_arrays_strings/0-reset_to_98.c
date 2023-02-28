d823ba6d1cdcb11a4b9d2d9c844865abc1481ee9
Stephen <stephenaluya@gmail.com>
2023-02-27 17:56:02 -0800

    Let's do pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,10 @@
-#include "main.c"
-#include <stdio.h>
-
+#include "main.h"
 /**                                                                                                                                                          
  * main - Main code
  * 
  * Return: Always 0.
  */
-int main(void)
+int reset_to_98(int *n)
 {
-reset_to_98(int *n);
 *n = 98;
 }
