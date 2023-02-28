2e4619dd1f06e511df2b45ddac8901fa7940ef83
Stephen <stephenaluya@gmail.com>
2023-02-28 13:24:32 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts2 - Main Code Insertion
+ * @str: char type pointer.
+ */
+void puts2(char *str)
+{
+int x = strlen(str);
+int i;
+for (i = 0; i < x; i++)
+{
+if(i == 0)
+{
+puts(str);
+}else if(i%2 == 0)
+{
+continue;
+}else
+{
+puts(str);
+}
+}
+puts('\n');
+}
