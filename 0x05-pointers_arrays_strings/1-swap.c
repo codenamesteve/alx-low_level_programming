278c60131a260f4500b4b6c08052b52a0fa7f356
Stephen <stephenaluya@gmail.com>
2023-02-27 18:17:28 -0800

    Another one

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - Main Code Insertion
+ * @a: int type pointer.
+ * @b: int type pointer.
+ */
+void swap_int(int *a, int *b);
+{
+x = *a;
+y = *b;
+*a = y;
+*b = x;
+}
