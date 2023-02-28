356f3173f1bee4736ea7eee261ec5eb133db76f4
Stephen <stephenaluya@gmail.com>
2023-02-28 06:51:56 -0800

    using putchar to reverse print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * print_rev - Main Code Insertion
+ * @str: int type pointer.
+ */
+void print_rev(char *str)
+{
+int x = strlen(str) - 1;
+int i;
+for (i = x; i >= 0; i--){
+_putchar(str[i]);
+}
+_putchar('\n');
+}
