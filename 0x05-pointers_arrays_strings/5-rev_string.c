c667de3a9e31be95e9a6d60dff025c3b2bc44952
Stephen <stephenaluya@gmail.com>
2023-03-01 04:38:11 -0800

    Reversing strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,12 +6,23 @@
  */
 void rev_string(char *s)
 {
-int length = strlen(s) - 1;
+/*int length = strlen(s) - 1;
 int i;
-char temp;
 for (i = length; i >= 0; i--)
 {
-temp = temp + (s[i]);
+s = putschar(s[i]);
+}*/
+int length = strlen(s);
+char *start = s;
+char *end = s + length - 1;
+char temp;
+
+while (start < end)
+{
+temp = *start;
+*start = *end;
+*end = temp;
+start++;
+end--;
 }
-putchar(temp);
 }
