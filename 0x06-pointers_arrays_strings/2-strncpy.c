90ba57282202ec2fc94e7eb30cff19c83f8df7a6
Stephen <stephenaluya@gmail.com>
2023-03-02 05:21:32 -0800

    string copy with pointers and fixed size

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strncpy - Main Code Insertion
+ * @dest: char type pointer.
+ * @src: char type pointer.
+ * @n: number of bytes
+ * Return: return value of dest.
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+char *dest_start = dest;
+int i = 0;
+while (*src != '\0' i < n)
+{
+*dest = *src;
+dest++;
+src++;
+i++;
+}
+*dest = '\0';
+return (dest_start);
+}
