d71e6a9a63a228a117ed1adc2afa9ee4fd5d19f2
Stephen <stephenaluya@gmail.com>
2023-03-03 09:52:36 -0800

    Leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * leet - Main Code Insertion
+ * @str: char type pointer.
+ * Return: return value of str.
+*/
+char *leet(char *str)
+{
+char *leet_chars = "431071";
+char *normal_chars = "aeotlAEOTL";
+    
+for (int i = 0; str[i] != '\0'; i++)
+{
+for (int j = 0; normal_chars[j] != '\0'; j++)
+{
+if (str[i] == normal_chars[j])
+{
+str[i] = leet_chars[j];
+break;
+}
+}
+}   
+return (str);
+}
