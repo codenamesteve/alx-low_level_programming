bd42b23baa56cb854136d62e18d809b6377f982f
Stephen <stephenaluya@gmail.com>
2023-03-03 07:35:09 -0800

    change case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * string_toupper - Main Code Insertion
+ * @str: char type pointer.
+ * Return: return value of str.
+*/
+char *string_toupper(char *)
+{
+int i;
+for (i = 0; i < strlen(str); i++)
+{
+str[i] = toupper(str[i]);
+}
+return str;
+}
