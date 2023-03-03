ba8c9a1add329b8eff0e63d1acc72e52a67a84eb
Stephen <stephenaluya@gmail.com>
2023-03-03 05:44:59 -0800

    reversing some more

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * reverse_array - Main Code Insertion
+ * @a: int type pointer.
+ * @n: int type counter.
+ */
+void reverse_array(int *a, int n)
+{
+int i, j, temp;
+for (i = 0, j = n - 1; i < j; i++, j--) {
+temp = a[i];
+a[i] = a[j];
+a[j] = temp;
+}
+}
