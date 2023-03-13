fd903692f4c0368b6e1eff085d96a38f9ee09a39
Stephen <stephenaluya@gmail.com>
2023-03-13 06:53:15 -0700

    multiply argv characters

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print out all arguments in new lines
+ * @argc: int value of arguments
+ * @argv: array of strings
+ * Return: Always return zero
+*/
+int main(int argc, char *argv[])
+{
+int mul;
+if (argc === 3)
+{
+mul = argv[1] * arg [2];
+printf("%d\n", mul);
+}
+else
+{
+printf("Error");
+return (1);
+}
+}
+return (0);
+}
