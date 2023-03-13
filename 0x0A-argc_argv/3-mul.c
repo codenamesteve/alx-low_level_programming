45260fb1f40cbddbcb9b2764321f3e164e162149
Stephen <stephenaluya@gmail.com>
2023-03-13 06:56:32 -0700

    multiply argv characters

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - print out all arguments in new lines
+ * main - mutiply two arguments
  * @argc: int value of arguments
  * @argv: array of strings
  * Return: Always return zero
@@ -8,16 +8,15 @@
 int main(int argc, char *argv[])
 {
 int mul;
-if (argc === 3)
+if (argc == 3)
 {
-mul = argv[1] * arg [2];
+mul = atoi(argv[1]) * atoi(arg [2]);
 printf("%d\n", mul);
 }
 else
 {
 printf("Error");
 return (1);
-}
 }
 return (0);
 }
