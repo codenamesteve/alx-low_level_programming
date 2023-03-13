88f318220b92127cf046d78b68e23236b5e2a76f
Stephen <stephenaluya@gmail.com>
2023-03-13 06:02:09 -0700

    argc and argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main - argc and argv function
  * @argc: int value of arguments
  * @argv: array of strings
  * Return: Always return zero
 **/
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-argc  __attribute__((unused));
 printf("%s\n", argv[0]);
 return (0);
 }
